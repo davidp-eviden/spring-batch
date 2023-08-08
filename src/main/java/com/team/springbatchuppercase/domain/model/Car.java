8661301f7e511fa7564a9acecceac5d657200548
David Poma <david.poma@atos.net>
2023-08-08 13:14:06 +0200

    Create the layout structure

--- src/main/java/com/team/springbatchuppercase/domain/model/Car.java
+++ src/main/java/com/team/springbatchuppercase/domain/model/Car.java
@@ -1,4 +1,26 @@
 package com.team.springbatchuppercase.domain.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "car")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Car {
+
+    @Id
+    @Column(name = "car_license_plate")
+    private String licensePlate;
+
+    @Column(name = "car_name")
+    private String name;
 }
--- src/main/java/com/team/springbatchuppercase/domain/model/CarTransformed.java
+++ src/main/java/com/team/springbatchuppercase/domain/model/CarTransformed.java
@@ -1,4 +1,26 @@
 package com.team.springbatchuppercase.domain.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Entity
+@Table(name = "car_uppercase")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class CarTransformed {
+    @Id
+    @Column(name = "car_license_plate")
+    private String licensePlate;
+
+    @Column(name = "car_name")
+    private String name;
 }
